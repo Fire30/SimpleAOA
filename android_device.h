12c5581782732c37df1d27a71e325185d8261d5f
Fire30 <tjcorley30@hotmail.com>
2015-05-27 00:31:08 -0400

    last formatting..

--- android_device.h
+++ android_device.h
@@ -32,35 +32,35 @@ THE SOFTWARE.
 #include <libusb.h>
 
 
-#define GOOGLE_VID						0x18D1
-#define ACCESSORY_PID_ADB				0x2D02
-#define ACCESSORY_PID_ADB				0x2D03
+#define GOOGLE_VID                      0x18D1
+#define ACCESSORY_PID_ADB               0x2D02
+#define ACCESSORY_PID_ADB               0x2D03
 
-#define FIRST_REQUEST_TYPE				0xC0
-#define ANDROID_REQUEST_TYPE			0x40
+#define FIRST_REQUEST_TYPE              0xC0
+#define ANDROID_REQUEST_TYPE            0x40
 
-#define ACCESSORY_GET_PROTOCOL			51
-#define ACCESSORY_SET_AUDIO_MODE		58
-#define ACCESSORY_START					53
-#define ACCESSORY_UNREGISTER_HID		54
-#define ACCESSORY_UNREGISTER_HID		55
-#define ACCESSORY_SET_HID_REPORT_DESC	56
-#define ACCESSORY_SEND_HID_EVENT		57
+#define ACCESSORY_GET_PROTOCOL          51
+#define ACCESSORY_SET_AUDIO_MODE        58
+#define ACCESSORY_START                 53
+#define ACCESSORY_UNREGISTER_HID        54
+#define ACCESSORY_UNREGISTER_HID        55
+#define ACCESSORY_SET_HID_REPORT_DESC   56
+#define ACCESSORY_SEND_HID_EVENT        57
 
-#define HID_EVENT_LEN					2
-#define CONSUMER_HID_DESC_LEN			23
+#define HID_EVENT_LEN                   2
+#define CONSUMER_HID_DESC_LEN           23
 
 // Various keycodes that I have used.
 // More can be found here:https://source.android.com/devices/input/keyboard-devices.html
-static const unsigned char KEYCODE_MEDIA_PLAY_PAUSE[]	= {0xCD, 0x00};
-static const unsigned char KEYCODE_VOLUME_MUTE[]		= {0xE2, 0x00};
-static const unsigned char KEYCODE_VOLUME_UP[]			= {0xE9, 0x00};
-static const unsigned char KEYCODE_VOLUME_DOWN[]		= {0xEA, 0x00};
-static const unsigned char KEYCODE_MEDIA_NEXT[]			= {0xB5, 0x00};
-static const unsigned char KEYCODE_MEDIA_PREVIOUS[]		= {0xB6, 0x00};
-static const unsigned char KEYCODE_MEDIA_STOP[]			= {0xB7, 0x00};
-static const unsigned char KEYCODE_MUSIC[]				= {0x83, 0x01};
-static const unsigned char HID_EVENT_RELEASE[]			= {0x00, 0x00};
+static const unsigned char KEYCODE_MEDIA_PLAY_PAUSE[]   = {0xCD, 0x00};
+static const unsigned char KEYCODE_VOLUME_MUTE[]        = {0xE2, 0x00};
+static const unsigned char KEYCODE_VOLUME_UP[]          = {0xE9, 0x00};
+static const unsigned char KEYCODE_VOLUME_DOWN[]        = {0xEA, 0x00};
+static const unsigned char KEYCODE_MEDIA_NEXT[]         = {0xB5, 0x00};
+static const unsigned char KEYCODE_MEDIA_PREVIOUS[]     = {0xB6, 0x00};
+static const unsigned char KEYCODE_MEDIA_STOP[]         = {0xB7, 0x00};
+static const unsigned char KEYCODE_MUSIC[]              = {0x83, 0x01};
+static const unsigned char HID_EVENT_RELEASE[]          = {0x00, 0x00};
 
 static unsigned char consumer_hid_descriptor[CONSUMER_HID_DESC_LEN] = {
     0x05, 0x0c,                     // Usage Page (Consumer Devices)
